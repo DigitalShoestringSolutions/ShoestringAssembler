c9b32c6815fea7c897a1527508d19d7a56a617a4
Toby Harris <51968582+tobyaharris@users.noreply.github.com>
2024-03-18 14:16:34 +0000

    Recipe reader and SM repo mirrordirector

---
+++ SMDownloader.py
@@ -0,0 +1,47 @@
+# SMDownloader.py
+
+# Given a recipe, downloads Service Modules according to mirrordirector.ServiceModuleURLs
+
+## -- Imports ---------------------------------------------------------------------
+
+# standard imports
+# none
+
+# installed imports
+import git # pip install GitPython
+
+# Local imports
+from mirrordirector import ServiceModuleURLs as SMURLs
+
+## --------------------------------------------------------------------------------
+
+recipefilename = "recipe.txt"
+
+with open(recipefilename, "r") as recipefile:
+        
+    for line in recipefile:
+
+        # Force reset
+        url = None # lest url be set in a previous loop, fails to update and is reused
+
+        # Skip line if blank or commented out python style
+        if line[0] in('\n',"#"):
+            continue
+
+        # split line into list of components
+        line = line.split("=")
+        line[-1] = line[-1].split("\n")[0]     # remove trailing newline from last item
+
+        # Associate names 
+        SMName = line[0]
+        branchname = line[1]
+
+        # Attempt to action recipe line
+        if SMName in SMURLs:
+            url = SMURLs[SMName]
+            #git.repo.clone_from(url)
+
+            print(SMName, url, branchname)
+
+        else:
+            print("Assembler Error: no Servie Module URL defined for line in recipe", line)
---
+++ mirrordirector.py
@@ -0,0 +1,15 @@
+# mirrordirector.py
+
+# Points to repositries of Shoestring Serivce Modules
+
+ServiceModuleURLs = {
+
+    "SetupLogging"  : "https://github.com/DigitalShoestringSolutions/SetupLogging",
+    "ReportGen"     : "https://github.com/DigitalShoestringSolutions/sm_ReportGeneration",
+    "AWSRelay"      : "https://github.com/DigitalShoestringSolutions/AWSRelay",
+    "Grafana"       : "https://github.com/DigitalShoestringSolutions/sm_grafana_ui",
+    "MQTT"          : "https://github.com/DigitalShoestringSolutions/sm_mqtt_broker",
+    "Timeseries"    : "https://github.com/DigitalShoestringSolutions/sm_timeseries_db",
+    "Sensing"       : "https://github.com/DigitalShoestringSolutions/sm_SensingDC"
+
+}
