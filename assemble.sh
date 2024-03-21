4725338abfe1d32caa447de21508bb172e6bf443
Toby Harris <51968582+tobyaharris@users.noreply.github.com>
2024-03-21 15:26:03 +0000

    Created init_SMs.py, which runs any init files in a service module when downloaded

--- assemble.sh
+++ assemble.sh
@@ -8,5 +8,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Download Service Modules into <solutionfiles>/ServiceModules
 python3 $SCRIPT_DIR/SMDownloader.py
 
+# Run init files in each service module, if present
+python3 $SCRIPT_DIR/init_SMs.py
+
 # Generate a docker-compose file at <solutionfiles>
 python3 $SCRIPT_DIR/include_docker_composes.py
---
+++ init_SMs.py
@@ -0,0 +1,35 @@
+# init_SMs.py
+
+# Permits Service Modules to request code to be run immediately after download, before compiling
+# eg SetupLogging would like setup_logging.sh to be run straight away, 
+#       such that during docker compose logs are redirected
+# Currently supported files types are .sh
+# File types to add: .py
+
+## -- Imports ---------------------------------------------------------------------
+
+# standard imports
+import os
+from pathlib import Path
+
+# installed imports
+#none
+
+# Local imports
+#none
+
+## --------------------------------------------------------------------------------
+
+## -- Firm-coded settings ---------------------------------------------------------
+
+ServiceModulesDir = Path(__file__).parents[2] # Assuming this script is in ServiceModules/Assembly/ShoestringAssembler/
+
+## --------------------------------------------------------------------------------
+
+## -- iterate over service module folders -----------------------------------------
+
+for file in ServiceModulesDir.rglob('*'):
+    if file.name in ['init_SM.sh']:
+        os.system(str(file))    # full abs path
+
+## --------------------------------------------------------------------------------
