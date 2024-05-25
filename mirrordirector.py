013bd162466138965af1ee609c6f44ee62bf7013
tobyaharris <51968582+tobyaharris@users.noreply.github.com>
2024-05-25 15:26:10 +0100

    Update mirrordirector.py
    Pointed Sensing to sm_SensingDC and added Docker

--- mirrordirector.py
+++ mirrordirector.py
@@ -8,11 +8,10 @@ ServiceModuleURLs = {
     "ReportGen"     : "https://github.com/DigitalShoestringSolutions/sm_ReportGeneration",
     "AWSRelay"      : "https://github.com/DigitalShoestringSolutions/AWSRelay",
     "Grafana"       : "https://github.com/DigitalShoestringSolutions/sm_grafana_ui",
-    "MQTT"          : "https://github.com/DigitalShoestringSolutions/sm_mqtt_broker",
+    "MQTTBroker"    : "https://github.com/DigitalShoestringSolutions/sm_mqtt_broker",
     "Timeseries"    : "https://github.com/DigitalShoestringSolutions/sm_timeseries_db",
     "Telemetry"     : "https://github.com/DigitalShoestringSolutions/Telemetry",
-
-    # temporary hack - use a branch of temperature in place of sm_sensing_dc, as the latter is still private
-    "Sensing"       : "https://github.com/DigitalShoestringSolutions/TemperatureMonitoringStarterSolution"
+    "Sensing"       : "https://github.com/DigitalShoestringSolutions/sm_SensingDC",
+    "Docker"        : https://github.com/DigitalShoestringSolutions/Docker"
 
 }
