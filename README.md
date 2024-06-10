4bd2bfb39e51211f14fc1bfca34099f81a3334aa
tobyaharris <51968582+tobyaharris@users.noreply.github.com>
2024-06-10 16:25:59 +0100

    Use SM's default branch when not supplied in recipe (#4)

--- README.md
+++ README.md
@@ -5,9 +5,31 @@ Future / WIP.
 The Solution is developed and delivered as a lightweight "recipe" of required Service Modules and configuration files.  
 An "assembler" then runs on the recipe and gathers the Service Modules from respective git repos.  
 
-<i>(As the workings of the assembler itself will need development, the assembler will have its own git repo - here. A minimal code snippet to download and run the assembler will be shipped with each solution, dubbed "preassembly.sh") </i>
+<i>(As the workings of the assembler itself will need development, the assembler will have its own git repo - here. A minimal code snippet to download and run the assembler will be shipped with each solution, dubbed "preassembly.sh") </i>  
 
-The assembler unites the config files with relevant Service Modules.  
+
+## Writing a recipe
+The recipe for the solution is a text file called `recipe.txt` in the Solution's root directory.  
+Service Modules are added to the Solution by appending their name to the recipe. A list of supported Service Modules can be found in `mirrordirector.py`.  
+A particular branch or tag of that Service Module's codebase can be used by adding `=branchname` after the name of the Service Module. If this is not supplied, that Service Module's default branch will be used.
+
+An example `recipe.txt`:
+```
+Grafana
+MQTTBroker=master
+Sensing=feature/recipe-lite
+```
+
+
+### Multiple of the same Service Module
+Multiple instances of the same Service Module are supported. Simply duplicate the lines in the recipe:
+```
+Sensing=feature/recipe-lite
+Sensing=feature/recipe-lite
+```
+This will create two Sensing Service Modules in your Solution. They can be on the same or different branches. 
+When assembled, the Sensing Service Module will be cloned into `ServiceModules/Sensing` and also `ServiceModules/Sensing2`
+
 
 ## To try it out:
 
--- SMDownloader.py
+++ SMDownloader.py
@@ -33,7 +33,7 @@ solution_files = Path(__file__).parents[3]
 
 ## -- Run -------------------------------------------------------------------------
 
-# keep a list of the instance names of service modules that have been downloaded, to manage duplicates
+# Keep a list of the instance names of service modules that have been downloaded, to manage duplicates.
 _downloaded_service_modules = []
 
 # Look for a recipe
@@ -41,8 +41,11 @@ with solution_files.joinpath(Path(recipefilename)).open(mode='r') as recipefile:
 
     for line in recipefile:
 
-        # Force reset
-        url = None # lest url be set in a previous loop, fails to update and is reused
+        # Force reset - lest any be set in a previous loop, fail to update and are reused.
+        sm_base_name = None
+        url = None
+        branch_name = None      # Also supports not supplying a branch name and using SM repo default.
+        _download_command = ""
 
         # Skip line if blank or commented out python style
         if line[0] in('\n',"#"):
@@ -50,33 +53,39 @@ with solution_files.joinpath(Path(recipefilename)).open(mode='r') as recipefile:
 
         # split line into list of components
         line = line.split("=")
-        line[-1] = line[-1].split("\n")[0]     # remove trailing newline from last item
+        line[-1] = line[-1].split("\n")[0]     # Remove trailing newline from last item.
 
         # Associate names
         sm_base_name = line[0]
-        branch_name = line[1]
+        if len(line) > 1:           # If an = was in the recipe line, try to use what follows as a branch/tab name.
+            branch_name = line[1]
 
         # Attempt to action recipe line
         if sm_base_name in ServiceModuleURLs:
             url = ServiceModuleURLs[sm_base_name]
 
             # Duplicate management
-            sm_instance_name = sm_base_name # First try to use the BaseName as instance name
+            sm_instance_name = sm_base_name # First try to use the base name as the instance name
             i = 1
             while sm_instance_name in _downloaded_service_modules:
                 i += 1                                              # If instance name taken, increment count
                 sm_instance_name = sm_base_name + str(i)            # and try using name with count eg Sensing2
             _downloaded_service_modules.append(sm_instance_name)    # record final instance name used
 
+            # Download with git clone
             download_to = str(solution_files.joinpath("ServiceModules/" + sm_instance_name))
-
             print()
             print("Downloading", sm_instance_name, "branch", branch_name, "from", url, "to", download_to)
-            os.system("git clone " + url + " -b " + branch_name + " " + download_to)
+            _download_command = "git clone " + url
+            if branch_name is not None:                     # If branch specified in recipe
+                _download_command += " -b " + branch_name   # Insert into the clone command. Else omit. 
+            _download_command += " " + download_to
+            os.system(_download_command)                    # Run the string concatenated above
 
 
         else:
             print()
             print("Assembler Error: no Servie Module URL defined for line in recipe", line)
+            print()
 
 ## --------------------------------------------------------------------------------
