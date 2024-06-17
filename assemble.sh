e6a5af05d3b7c984eaaa0e4d93903549ec2c9c85
Toby Harris <51968582+tobyaharris@users.noreply.github.com>
2024-06-17 17:48:21 +0100

    Added link_config to assemble.sh ahead of SMinit

--- assemble.sh
+++ assemble.sh
@@ -8,6 +8,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Download Service Modules into <solutionfiles>/ServiceModules
 python3 $SCRIPT_DIR/SMDownloader.py
 
+# Link config files between UserConfig and each Service Module's config dirctory
+python3 $SCRIPT_DIR/link_config.py
+
 # Run init files in each service module, if present
 python3 $SCRIPT_DIR/init_SMs.py
 
