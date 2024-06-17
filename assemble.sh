b879bc3842827704b6a9d8cc16a1857e76579c4c
Toby Harris <51968582+tobyaharris@users.noreply.github.com>
2024-06-17 18:53:46 +0100

    Moved config linking to after init files

--- assemble.sh
+++ assemble.sh
@@ -8,11 +8,11 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Download Service Modules into <solutionfiles>/ServiceModules
 python3 $SCRIPT_DIR/SMDownloader.py
 
-# Link config files between UserConfig and each Service Module's config dirctory
-python3 $SCRIPT_DIR/link_config.py
-
 # Run init files in each service module, if present
 python3 $SCRIPT_DIR/init_SMs.py
+
+# Link config files between UserConfig and each Service Module's config dirctory
+python3 $SCRIPT_DIR/link_config.py
 
 # Generate a docker-compose file at <solutionfiles>
 python3 $SCRIPT_DIR/include_docker_composes.py
