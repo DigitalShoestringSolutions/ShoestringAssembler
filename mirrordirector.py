33816dd3ed28f30881eb251f3b104533ead1586e
tobyaharris <51968582+tobyaharris@users.noreply.github.com>
2024-08-29 14:49:14 +0100

    Added alternate full name for ReportGeneration, increased indentation

--- mirrordirector.py
+++ mirrordirector.py
@@ -5,22 +5,23 @@
 ServiceModuleURLs = {
 
     # Infrastructure modules
-    "Docker"        : "https://github.com/DigitalShoestringSolutions/Docker",
-    "SetupLogging"  : "https://github.com/DigitalShoestringSolutions/SetupLogging",
+    "Docker"            : "https://github.com/DigitalShoestringSolutions/Docker",
+    "SetupLogging"      : "https://github.com/DigitalShoestringSolutions/SetupLogging",
 
     # Data collection modules
-    "Sensing"       : "https://github.com/DigitalShoestringSolutions/sm_SensingDC",
+    "Sensing"           : "https://github.com/DigitalShoestringSolutions/sm_SensingDC",
 
     # Data transfer modules
-    "MQTTBroker"    : "https://github.com/DigitalShoestringSolutions/sm_mqtt_broker",
+    "MQTTBroker"        : "https://github.com/DigitalShoestringSolutions/sm_mqtt_broker",
 
     # Data storage modules
-    "InfluxDB"    : "https://github.com/DigitalShoestringSolutions/sm_timeseries_db",
+    "InfluxDB"          : "https://github.com/DigitalShoestringSolutions/sm_timeseries_db",
 
     # User interface modules
-    "Grafana"       : "https://github.com/DigitalShoestringSolutions/sm_grafana_ui",
+    "Grafana"           : "https://github.com/DigitalShoestringSolutions/sm_grafana_ui",
 
     # Data output modules
-    "ReportGen"     : "https://github.com/DigitalShoestringSolutions/sm_ReportGeneration",
-    "AWSRelay"      : "https://github.com/DigitalShoestringSolutions/AWSRelay",
+    "ReportGen"         : "https://github.com/DigitalShoestringSolutions/sm_ReportGeneration",
+    "ReportGeneration"  : "https://github.com/DigitalShoestringSolutions/sm_ReportGeneration",
+    "AWSRelay"          : "https://github.com/DigitalShoestringSolutions/AWSRelay",
 }
