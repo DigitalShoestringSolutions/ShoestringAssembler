caf366bbf995dd75ad5d08d6c7b331a979e5b51a
tobyaharris <51968582+tobyaharris@users.noreply.github.com>
2024-10-08 15:57:44 +0100

    Print the Assembler version
    Not sure if this will work - expecting the command will run from cwd, aka solution files.

--- assemble.sh
+++ assemble.sh
@@ -2,15 +2,20 @@
 
 # Top level of Shoestring Assembler
 
+# Save some file paths as constants
 # Get the parent dir of this script
 SCRIPT_DIR="$(dirname -- "$(realpath -- "$0")")"
 
 # Get the parent dir of SCRIPT_DIR
 SCRIPT_DIR_DIR="$(dirname -- "$(realpath -- $SCRIPT_DIR)")"
 
-# Define a 
+# Define once the log file name, as a full path
 LOG_FILE=$SCRIPT_DIR_DIR/assemblerlog.txt
 
+
+# Run the Assembler!
+# Print the version of the Assembler being used
+echo "git log --oneline -1" 2>&1 | tee -a $LOG_FILE
 
 # Download Service Modules into <solutionfiles>/ServiceModules
 python3 $SCRIPT_DIR/SMDownloader.py 2>&1 | tee -a $LOG_FILE
