361b7afeccf5162ee91ccb21649fcc3df97a741b
tobyaharris <51968582+tobyaharris@users.noreply.github.com>
2024-10-08 15:27:14 +0100

    Added tiny sleeps in assemble.sh
    To address an issue of some stderr prints coming several lines early, introduced in this branch.

--- assemble.sh
+++ assemble.sh
@@ -10,12 +10,16 @@ PARENT_DIR="$(dirname -- "$(realpath -- $SCRIPT_DIR)")"
 
 # Download Service Modules into <solutionfiles>/ServiceModules
 python3 $SCRIPT_DIR/SMDownloader.py 2>&1 | tee -a $PARENT_DIR/assemblerlog.txt
+sleep .1
 
 # Run init files in each service module, if present
 python3 $SCRIPT_DIR/init_SMs.py 2>&1 | tee -a $PARENT_DIR/assemblerlog.txt
+sleep .1
 
 # Link config files between UserConfig and each Service Module's config dirctory
 python3 $SCRIPT_DIR/link_config.py 2>&1 | tee -a $PARENT_DIR/assemblerlog.txt
+sleep .1
 
 # Generate a docker-compose file at <solutionfiles>
 python3 $SCRIPT_DIR/include_docker_composes.py 2>&1 | tee -a $PARENT_DIR/assemblerlog.txt
+sleep .1
