3862c6cf24aea0f536683ada76ff8d6552a1028b
tobyaharris <51968582+tobyaharris@users.noreply.github.com>
2024-10-08 15:03:57 +0100

    Put logs next to assemble.sh

--- assemble.sh
+++ assemble.sh
@@ -6,13 +6,13 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Download Service Modules into <solutionfiles>/ServiceModules
-python3 $SCRIPT_DIR/SMDownloader.py 2>&1 | tee -a assemblerlog.txt
+python3 $SCRIPT_DIR/SMDownloader.py 2>&1 | tee -a $SCRIPT_DIR/assemblerlog.txt
 
 # Run init files in each service module, if present
-python3 $SCRIPT_DIR/init_SMs.py 2>&1 | tee -a assemblerlog.txt
+python3 $SCRIPT_DIR/init_SMs.py 2>&1 | tee -a $SCRIPT_DIR/assemblerlog.txt
 
 # Link config files between UserConfig and each Service Module's config dirctory
-python3 $SCRIPT_DIR/link_config.py 2>&1 | tee -a assemblerlog.txt
+python3 $SCRIPT_DIR/link_config.py 2>&1 | tee -a $SCRIPT_DIR/assemblerlog.txt
 
 # Generate a docker-compose file at <solutionfiles>
-python3 $SCRIPT_DIR/include_docker_composes.py 2>&1 | tee -a assemblerlog.txt
+python3 $SCRIPT_DIR/include_docker_composes.py 2>&1 | tee -a $SCRIPT_DIR/assemblerlog.txt
