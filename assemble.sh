4945547b3092c3b00c0a36246f51cbf8ecb36889
tobyaharris <51968582+tobyaharris@users.noreply.github.com>
2024-10-08 15:34:03 +0100

    Extended sleeps considerably
    Sequence issue still present with 0.1 gaps

--- assemble.sh
+++ assemble.sh
@@ -10,16 +10,16 @@ SCRIPT_DIR_DIR="$(dirname -- "$(realpath -- $SCRIPT_DIR)")"
 
 # Download Service Modules into <solutionfiles>/ServiceModules
 python3 $SCRIPT_DIR/SMDownloader.py 2>&1 | tee -a $SCRIPT_DIR_DIR/assemblerlog.txt
-sleep .1
+sleep 3
 
 # Run init files in each service module, if present
 python3 $SCRIPT_DIR/init_SMs.py 2>&1 | tee -a $SCRIPT_DIR_DIR/assemblerlog.txt
-sleep .1
+sleep 3
 
 # Link config files between UserConfig and each Service Module's config dirctory
 python3 $SCRIPT_DIR/link_config.py 2>&1 | tee -a $SCRIPT_DIR_DIR/assemblerlog.txt
-sleep .1
+sleep 3
 
 # Generate a docker-compose file at <solutionfiles>
 python3 $SCRIPT_DIR/include_docker_composes.py 2>&1 | tee -a $SCRIPT_DIR_DIR/assemblerlog.txt
-sleep .1
+sleep 3
