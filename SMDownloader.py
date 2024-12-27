ad939bd645cc0bfe429bc8ea743f260a012f68e0
Toby Harris <51968582+tobyaharris@users.noreply.github.com>
2024-12-27 16:28:39 +0000

    Replaced wording of "branch" with "version", as it will mostly be used with tags

--- SMDownloader.py
+++ SMDownloader.py
@@ -51,13 +51,13 @@ with solution_files.joinpath(Path(recipefilename)).open(mode='r') as recipefile:
         # Force reset - lest any be set in a previous loop, fail to update and are reused.
         sm_base_name = None
         url = None
-        branch_specifier = None      # Also supports not supplying a branch name and using SM repo default.
-        _branch_specifier_search = None
+        version_specifier = None      # Also supports not supplying a branch/tag name and using SM repo default.
+        _version_specifier_search = None
         _available_branches = None
         _available_long_heads = None
         _available_tags = None
         _available_long_tags = None
-        _download_branch = None
+        _download_version = None
         _download_hash = None
         _download_command = ""
 
@@ -71,8 +71,8 @@ with solution_files.joinpath(Path(recipefilename)).open(mode='r') as recipefile:
 
         # Associate names
         sm_base_name = line[0]
-        if len(line) > 1:           # If an = was in the recipe line, try to use what follows as a branch/tag name.
-            branch_specifier = line[1]
+        if len(line) > 1:               # If an = was in the recipe line, try to use what follows as a branch/tag name,
+            version_specifier = line[1] # else the default value of None will persist
 
         # Attempt to action recipe line
         if sm_base_name in ServiceModuleURLs:
@@ -90,7 +90,7 @@ with solution_files.joinpath(Path(recipefilename)).open(mode='r') as recipefile:
             # To remove the possibility of ending up with the wrong version downloaded,
             # ensure the target branch or tag is available before attempting clone.
 
-            if branch_specifier is not None:
+            if version_specifier is not None:
 
                 # Get list of remote branches
                 _available_long_heads = os.popen("git ls-remote --heads " + url).read().splitlines()
@@ -99,8 +99,8 @@ with solution_files.joinpath(Path(recipefilename)).open(mode='r') as recipefile:
                     _available_branches.append(head.split("heads/")[-1])    # Everything after heads/ , ie the name of the branch
 
                 # First attempt to clone an exact match of a branch name:
-                if branch_specifier in _available_branches:
-                    _download_branch = branch_specifier
+                if version_specifier in _available_branches:
+                    _download_version = version_specifier
 
                 else:
                     # Get list of remote tags, sorted semver highest to lowest.
@@ -110,8 +110,8 @@ with solution_files.joinpath(Path(recipefilename)).open(mode='r') as recipefile:
                         _available_tags.append(tag.split("tags/")[-1])      # Everything after tags/ , ie the name of the tag
 
                     # If an exact tag match is available, take it
-                    if branch_specifier in _available_tags:
-                        _download_branch = branch_specifier
+                    if version_specifier in _available_tags:
+                        _download_version = version_specifier
 
                     # If no exact branch or tag match, search for the tag that is the best semver match.
                     else:
@@ -119,41 +119,41 @@ with solution_files.joinpath(Path(recipefilename)).open(mode='r') as recipefile:
                         # It should not begin with a digit (e.g. if I asked for v1.6 I don't want v1.62)
                         # It should not begin with a - (as I desire to exclude prereleases from search, partially because git ls-remote can't get them in the semver order).
                         # Hence, the only acceptable next character is a full stop. Add this to the search term to exclude the above alternatives.
-                        _branch_specifier_search = branch_specifier + '.'
+                        _version_specifier_search = version_specifier + '.'
 
                         for tag in _available_tags:
                             # _available_tags is already sorted by semver M.m.p highest precedence first when created.
-                            if tag.startswith(_branch_specifier_search):
+                            if tag.startswith(_version_specifier_search):
 
                                 # Ignore prerelease tags
                                 # A request for v1.10 could so far pick up v1.10.2-rc3
                                 # Furthermore, it could select v1.10.2-rc3 over v1.10.2 due to ls-remote's imperfect ordering.
                                 # However, it would be excessive to ignore any tag with a dash in it. lite-v1.2.3 is a desired pickup from specifier lite-v1.2
-                                _suffix = tag[len(_branch_specifier_search):]
+                                _suffix = tag[len(_version_specifier_search):]
                                 if '-' in _suffix:  # if there is a dash in the unspecified part of the tag
                                     continue        # skip and continue search
 
-                                _download_branch = tag
+                                _download_version = tag
                                 break
 
-                        # if _download_branch is still None, a suitable branch/tag could not be found.
-                        if _download_branch is None: # not acceptable here as within `if branch_specifier is not None:` far above.
-                            print("    ERROR: No suitable branch of", sm_base_name, "found for specifier", branch_specifier, "Cancelling download of", sm_instance_name)
+                        # if _download_version is still None, a suitable branch/tag could not be found.
+                        if _download_version is None: # not acceptable here as within `if branch_specifier is not None:` far above.
+                            print("    ERROR: No suitable branch of", sm_base_name, "found for specifier", version_specifier, "Cancelling download of", sm_instance_name)
                             continue    # give up on this line of the recipe and move on to next
 
                 # Get the short commit hash from branch or tag name
-                _download_hash = os.popen("git ls-remote " + url + " " + _download_branch).read()[:7]
+                _download_hash = os.popen("git ls-remote " + url + " " + _download_version).read()[:7]
 
             # Download with git clone
             download_to = str(solution_files.joinpath("ServiceModules/" + sm_instance_name))
             print()
-            print("    Downloading", sm_instance_name, "branch", _download_branch, "(hash", _download_hash + ")", "from specifier", branch_specifier)
+            print("    Downloading", sm_instance_name, "version", _download_version, "(hash", _download_hash + ")", "from specifier", version_specifier)
             print("        from", url)
             print("        to  ", download_to)
 
             _download_command = "git clone --quiet " + url
-            if _download_branch is not None:                    # If branch specified in recipe
-                _download_command += " -b " + _download_branch  # Insert into the clone command. Else omit.
+            if _download_version is not None:                    # If branch specified in recipe
+                _download_command += " -b " + _download_version  # Insert into the clone command. Else omit.
             _download_command += " " + download_to
 
             os.system(_download_command)                        # Run the string concatenated above
